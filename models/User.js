4aff1f18a99464aedebcee8ce3e78bbc94b89bf4
Marcin <marcinchodkowski004@wp.pl>
2024-07-07 21:31:50 +0200

    pushing hashed password to database

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose')
-const { isEmail } = require('validator')
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
 	email: {
@@ -11,10 +12,17 @@ const userSchema = new mongoose.Schema({
 	},
 	password: {
 		type: String,
+		unique: false,
 		required: [true, 'Please enter an password'],
 		minlength: [6, 'Minumum password length is 6 characters'],
 	},
-})
+});
 
-const User = mongoose.model('user', userSchema)
-module.exports = User
+// fire a func before doc saved to DB
+userSchema.pre('save', async function () {
+	const salt = await bcrypt.genSalt();
+	this.password = await bcrypt.hash(this.password, salt);
+});
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
