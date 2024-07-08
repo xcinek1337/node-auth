d195e032ada75dc8500ab30c750d8b73699befc8
Marcin <marcinchodkowski004@wp.pl>
2024-07-08 20:40:40 +0200

    dd middleware to check user authentication and provide user data to UI

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,3 +71,10 @@ module.exports.login_post = async (req, res) => {
 		res.status(400).json({ errors });
 	}
 };
+
+module.exports.logout_get = async (req, res) => {
+	try {
+		res.cookie('jwt', '', { maxAge: 1 });
+		res.redirect('/');
+	} catch (error) {}
+};
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
 	const token = req.cookies.jwt;
@@ -15,4 +16,28 @@ const requireAuth = (req, res, next) => {
 		res.redirect('/login');
 	}
 };
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+	const token = req.cookies.jwt;
+	if (token) {
+		jwt.verify(token, 'non visible secret', async (err, decodedToken) => {
+			if (err) {
+				console.log('errpr', err.message);
+				res.locals.user = null;
+				next();
+			} else {
+				console.log('dziala', decodedToken);
+				let user = await User.findById(decodedToken.id);
+				res.locals.user = user;
+
+				next();
+			}
+		});
+	} else {
+		res.locals.user = null;
+		next();
+	}
+};
+
+module.exports = { requireAuth, checkUser };
